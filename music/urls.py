0811e0b91fb171645a70b9e3cd7e309b6a464eb7
tanishgupta-git <tanishgupta8181@gmail.com>
2020-09-13 13:10:09 +0530

    added delete and update option of albums

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,7 @@ app_name = 'music'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='music'),
     path('<int:pk>/', views.DetailView.as_view(), name="detail"),
-    path('album/add', views.AlbumCreate.as_view(), name='album-add')
+    path('album/add', views.AlbumCreate.as_view(), name='album-add'),
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+    path('album/<int:pk>/delete', views.AlbumDelete.as_view(), name='album-delete')
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Album
 
 # Create your views here.
@@ -19,3 +20,10 @@ class AlbumCreate(CreateView):
     model = Album
     fields = ['artist','album_title','genre','album_logo']
 
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:music')
