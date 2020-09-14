97e916ae2c9b52a8399cc5afd4f0d891662355b2
tanishgupta-git <tanishgupta8181@gmail.com>
2020-09-14 18:13:01 +0530

    added authentication for users

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'music'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='music'),
+    path('register/', views.UserFormView.as_view(), name='register'),
     path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     path('album/add', views.AlbumCreate.as_view(), name='album-add'),
     path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
